4592816fd890aef130c1dc9a61f9c64648327775
Artem Rashev <mgpnd.personal@gmail.com>
2021-09-05 12:01:42 +0300

    Fill database with pokemons

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+pokemon_csv_path = File.join(File.dirname(__FILE__), './seeds/pokemon.csv')
+pokemon_data = CSV.read(pokemon_csv_path)
+
+pokemon_entities = pokemon_data[1..-1].map do |line|
+  _,
+  name,
+  type1,
+  type2,
+  total,
+  hp,
+  attack,
+  defense,
+  sp_atk,
+  sp_def,
+  speed,
+  gen,
+  legendary \
+    = line
+
+  {
+    name: name,
+    type1: type1,
+    type2: type2,
+    total_score: total,
+    hp: hp,
+    attack: attack,
+    defense: defense,
+    special_attack: sp_atk,
+    special_defense: sp_def,
+    speed: speed,
+    generation: gen,
+    legendary: legendary
+  }
+end
+
+Pokemon.insert_all pokemon_entities
