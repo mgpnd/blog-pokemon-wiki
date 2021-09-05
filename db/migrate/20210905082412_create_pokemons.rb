93c6401cdfdb7ddf204c48c9dcb572e88969908f
Artem Rashev <mgpnd.personal@gmail.com>
2021-09-05 11:37:05 +0300

    Add pokemons table

---
+++ db/migrate/20210905082412_create_pokemons.rb
@@ -0,0 +1,21 @@
+class CreatePokemons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pokemons do |t|
+      t.string :type1, null: true
+      t.string :type2, null: true
+
+      t.integer :hp, null: false
+      t.integer :attack, null: false
+      t.integer :defense, null: false
+      t.integer :special_attack, null: false
+      t.integer :special_defence, null: false
+      t.integer :speed, null: false
+      t.integer :total_score, null: false
+
+      t.integer :generation, null: false
+      t.boolean :legendary, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
